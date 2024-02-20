b33243282ca82fac3810c8a9b8f945fd154c5f0d
pedroshaina <pedroshaina_@hotmail.com>
2024-02-20 20:06:12 -0300

    feat: initial project structure

---
+++ src/main/java/com/hostfully/technicalchallenge/TechnicalChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.hostfully.technicalchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalChallengeApplication.class, args);
+	}
+
+}
